153ab90206fcf441a5b6fe904fd8a6951aae6f57
boghdady <boghdady107@gmail.com>
2020-01-24 18:46:10 +0200

    init

---
+++ lib/utils/view/router.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_shop_app/utils/view/screen_args/product_details_args.dart';
+import 'package:firebase_shop_app/view/screens/cart_screen.dart';
+import 'package:firebase_shop_app/view/screens/product_details_screen.dart';
+import 'package:flutter/material.dart';
+import '../../utils/view/constant_routs.dart';
+import '../../view/screens/products_overview_screen.dart';
+import '../../view/screens/un_defined_screen.dart';
+
+class Router {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case productsOverviewRoute:
+        return MaterialPageRoute(builder: (_) => ProductsOverviewScreen());
+      case productDetailsRoute:
+        ProductDetailsArgs args = settings.arguments;
+        return MaterialPageRoute(
+            builder: (_) => ProductDetailsScreen(
+                  productId: args.id,
+                  index: args.index,
+                ));
+      case cartScreenRoute:
+        return MaterialPageRoute(builder: (_) => CartScreen());
+      default:
+        return MaterialPageRoute(
+          builder: (context) => UndefinedScreen(
+            name: settings.name,
+          ),
+        );
+    }
+  }
+}
---
+++ lib/view/widgets/cart_item_widget.dart
@@ -0,0 +1,68 @@
+import 'package:firebase_shop_app/core/providers/cart_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CartItemWidget extends StatelessWidget {
+  final String itemId;
+  final String productId;
+  final double price;
+  final int quantity;
+  final String title;
+
+  CartItemWidget({
+    this.itemId,
+    this.productId,
+    this.price,
+    this.quantity,
+    this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(itemId),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        // Using Provider.of not Consumer because Provider.of work as a method
+        Provider.of<CartProvider>(context, listen: false)
+            .removeItemFromCart(itemId);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: FittedBox(
+                  child: Text('\$$price'),
+                ),
+              ),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            trailing: Text('$quantity x'),
+          ),
+        ),
+      ),
+    );
+  }
+}
